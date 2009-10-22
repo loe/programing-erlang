20610e49deddcadf21eaf00dd86ab9eae6a4efb4
W. Andrew Loe III <andrew@andrewloe.com>
2009-10-21 23:56:10 -0700

    We can totally do squares too.

--- geometry.erl
+++ geometry.erl
@@ -2,4 +2,5 @@
 -export([area/1]).
 
 area({rectangle, Width, Ht})  -> Width * Ht;
-area({circle, R})             -> 3.14159 * R * R.
+area({circle, R})             -> 3.14159 * R * R;
+area({square, Side})          -> Side * Side.
