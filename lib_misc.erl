46cd4079d7cd5b01ab6780599e0ead58e45ff9c5
W. Andrew Loe III <andrew@andrewloe.com>
2010-01-12 21:52:40 -0800

    Pythagorean theorem anyone?

--- lib_misc.erl
+++ lib_misc.erl
@@ -1,5 +1,6 @@
 -module(lib_misc).
--export([for/3, qsort/1]).
+-export([for/3, qsort/1, pythag/1]).
+-import(lists, [seq/1]).
 
 for(Max, Max, F)  ->  [F(Max)];
 for(I, Max, F)    ->  [F(I)|for(I+1, Max, F)].
@@ -10,3 +11,13 @@ qsort ([Pivot|T]) ->
   qsort([X || X <- T, X < Pivot])
   ++ [Pivot] ++
   qsort([X || X <- T, X >= Pivot]).
+  
+pythag (N) ->
+  [ 
+    {A, B, C} ||
+    A <- lists:seq(1,N),
+    B <- lists:seq(1, N),
+    C <- lists:seq(1, N),
+    A + B + C =< N,
+    A*A + B*B =:= C*C
+  ].
